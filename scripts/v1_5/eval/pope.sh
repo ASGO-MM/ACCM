4853386000598bbab97d09750d339ce635565535
fmy7834 <80671862+fmy7834@users.noreply.github.com>
2025-08-11 18:04:38 +0800

    Update pope.sh

--- scripts/v1_5/eval/pope.sh
+++ scripts/v1_5/eval/pope.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CKPT_NAME=llava-v1.5-7b_36-tokens_scene-graph
+CKPT_NAME=llava-v1.5-7b_36-tokens
 MODEL_PATH=/home/fmy/data/llava-v1.5-7b
 
 export CUDA_VISIBLE_DEVICES=1
@@ -10,13 +10,10 @@ python -m llava.eval.model_vqa_loader \
     --image-folder /home/fmy/data/LLaVA-main/playground/data/eval/pope/val2014 \
     --answers-file /home/fmy/data/LLaVA-main/playground/data/eval/pope/answers/$CKPT_NAME.jsonl \
     --temperature 0 \
-    --conv-mode vicuna_v1 \
-    --add_proto false \
-    --proto_num 0
+    --conv-mode vicuna_v1
 
 python llava/eval/eval_pope.py \
     --annotation-dir /home/fmy/data/LLaVA-main/playground/data/eval/pope/coco \
     --question-file /home/fmy/data/LLaVA-main/playground/data/eval/pope/llava_pope_test.jsonl \
     --result-file /home/fmy/data/LLaVA-main/playground/data/eval/pope/answers/$CKPT_NAME.jsonl
 
-# --model-base /public/home/renwu04/fmy/data/vicuna-7b-v1.5 \
