eb50b088a2db4b43510e05f8b01687ba60ec2608
fmy7834 <80671862+fmy7834@users.noreply.github.com>
2025-08-11 18:07:46 +0800

    Update mmvp.sh

--- scripts/v1_5/eval/mmvp.sh
+++ scripts/v1_5/eval/mmvp.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-CKPT_NAME=llava-v1.5-7b_short-caption-clipcap_question_72-tokens_wo-normalize+selector_4400
+CKPT_NAME=llava-v1.5-7b_short-caption-clipcap_72-tokens
 MODEL_PATH=/home/fmy/data/llava-v1.5-7b
 
 export CUDA_VISIBLE_DEVICES=0
 python -W ignore llava/eval/mmvp/mmvp_eval.py --model_path $MODEL_PATH \
-   --answers_file llava/eval/mmvp/answers/$CKPT_NAME.jsonl \
-   --add_proto false \
-   --proto_num 0
+   --answers_file llava/eval/mmvp/answers/$CKPT_NAME.jsonl 
 
 python llava/eval/mmvp/mmvp_test.py --answers_file llava/eval/mmvp/answers/${CKPT_NAME}_0.jsonl \
    --output_file llava/eval/mmvp/answers/${CKPT_NAME}_incorrect.jsonl \
