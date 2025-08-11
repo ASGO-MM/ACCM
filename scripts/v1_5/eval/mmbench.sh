c578df5fdf7f6564de88d36b9be295c26de52329
fmy7834 <80671862+fmy7834@users.noreply.github.com>
2025-08-11 18:06:57 +0800

    Update mmbench.sh

--- scripts/v1_5/eval/mmbench.sh
+++ scripts/v1_5/eval/mmbench.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CKPT_NAME=llava-v1.5-7b_short-caption-clipcap_72-tokens+selector_4400
+CKPT_NAME=llava-v1.5-7b_short-caption-clipcap_72-tokens
 MODEL_PATH=/home/fmy/data/llava-v1.5-7b
 
 SPLIT="mmbench_dev_20230712"
@@ -12,9 +12,7 @@ python -m llava.eval.model_vqa_mmbench \
     --answers-file /home/fmy/data/LLaVA-main/playground/data/eval/mmbench/answers/$SPLIT/$CKPT_NAME.jsonl \
     --single-pred-prompt \
     --temperature 0 \
-    --conv-mode vicuna_v1 \
-    --add_proto false \
-    --proto_num 0
+    --conv-mode vicuna_v1
 
 mkdir -p /home/fmy/data/LLaVA-main/playground/data/eval/mmbench/answers_upload/$SPLIT
 
@@ -24,5 +22,3 @@ python scripts/convert_mmbench_for_submission.py \
     --upload-dir /home/fmy/data/LLaVA-main/playground/data/eval/mmbench/answers_upload/$SPLIT \
     --experiment $CKPT_NAME
 
-
-#--model-base /public/home/renwu04/fmy/data/vicuna-7b-v1.5 \
